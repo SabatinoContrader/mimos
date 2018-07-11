acbc3c46147d74c15d98fc0b2e90d24f5a218c67
Mario Saponara <mario.saponara@ext.grupposandonato.it>
2018-07-11 11:34:48 +0200

    newProject

---
+++ src/main/java/com/virtualpairprogrammers/servlets/UtenteServlet.java
@@ -0,0 +1,18 @@
+package com.virtualpairprogrammers.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UtenteServlet {
+
+    public void service (HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException,IOException
+    {
+
+    }
+}
+
+
